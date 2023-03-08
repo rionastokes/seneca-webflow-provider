03bc6ef3d0010c48f153a9f3363c0d3f3c6dc74a
Ríona Stokes <rionastokes@gmail.com>
2023-03-09 00:07:27 +0100

    Capitalise constants from require

--- test/live.js
+++ test/live.js
@@ -1,11 +1,11 @@
 const Webflow = require('webflow-api')
-const token = require('./local-env').WEBFLOW_ACCESSTOKEN
+const Token = require('./local-env').WEBFLOW_ACCESSTOKEN
 
 run()
 
 async function run() {
   // initialize the client with the access token
-  const webflow = new Webflow({ token })
+  const webflow = new Webflow({ Token })
 
   const sites = await webflow.sites()
   console.log(sites)
